7dde5af0d7f14c9d7607d2e7e71f094880169632
arodko <arodko@POLSAGE02>
2022-05-02 18:28:45 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalReportsSourceChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
