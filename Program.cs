e9851d71fae244a1af7dde07dcfb439a8fe44a52
Andrew Rodko <itadmin@polanerselections.com>
2022-05-09 16:33:46 +0000

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
 
 namespace CrystalReportsSourceChange
 {
     class Program
     {
+        private const string ReportDirectory = "\\\\polsage01.aws.polaner.com\\Sage\\Sage 100 Premium\\MAS90\\Reports";
+        private const string TempDirectory = "\\\\polsage01.aws.polaner.com\\Sage\\Sage 100 Premium\\MAS90\\Reports\\temp";
+        private static string[] fileEntries;
+        private const string DatabaseName = "polsql01.aws.polaner.com";
+
         static void Main(string[] args)
         {
+            GetFileList();
+            ProcessFiles();
+        }
+
+        private static void ProcessFiles()
+        {
+            
+            foreach (string file in fileEntries)
+            {
+                ReportDocument report = LoadReport(file);
+                foreach(Table table in report.Database.Tables)
+                {
+                    ChangeServerName(table);
+                }
+                SaveReport(report, file);
+            }
+        }
+
+        private static void SaveReport(ReportDocument report, string file)
+        {
+            report.SaveAs(file);
+            report.Dispose();
+        }
+
+        private static void ChangeServerName(Table table)
+        {
+            TableLogOnInfo logOnInfo = new TableLogOnInfo();
+            ConnectionInfo connectionInfo = new ConnectionInfo();
+            logOnInfo = table.LogOnInfo;
+            connectionInfo = logOnInfo.ConnectionInfo;
+            connectionInfo.ServerName = DatabaseName;
+            connectionInfo.IntegratedSecurity = true;
+            table.ApplyLogOnInfo(logOnInfo);
+        }
+
+        private static ReportDocument LoadReport(string file)
+        {
+            ReportDocument report = new ReportDocument();
+            report.Load(file);
+            return report;
+        }
+
+        private static void GetFileList()
+        {
+            fileEntries = Directory.GetFiles(ReportDirectory, "*.rpt");
         }
     }
 }
